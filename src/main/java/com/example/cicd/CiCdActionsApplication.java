6cc4559179e21d8c8c852a98b2ccf2ff68256af7
nkazbekov23 <nursultankazbekov23@gmail.com>
2022-01-08 11:32:00 +0600

    init git

---
+++ src/main/java/com/example/cicd/CiCdActionsApplication.java
@@ -0,0 +1,21 @@
+package com.example.cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class CiCdActionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CiCdActionsApplication.class, args);
+    }
+
+    @GetMapping
+    public String welcome() {
+        return "Welcome to ci cd";
+    }
+
+}
